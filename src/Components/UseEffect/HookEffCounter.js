b278d63c0755a554257e8607e0cd4a8ffd28a2c8
Divy Parekh <divyparekh1810@gmail.com>
2022-04-28 18:01:57 +0530

    UseEffect Dependency is Done

--- src/Components/UseEffect/HookEffCounter.js
+++ src/Components/UseEffect/HookEffCounter.js
@@ -3,12 +3,15 @@ import React,{useEffect, useState} from "react";
 const HookEffCounter = () => {
 
     const [ count, setCount] = useState(0);
+    const [name, setName] = useState('');
 
     useEffect(() => {
+        console.log('updating');
         document.title = `You Clicked ${count} times`
-    })
+    }, [count])
     return(
         <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
             <button onClick={() => setCount(prevValue => prevValue + 1)}>Click {count} Times</button>
         </div>
     )
